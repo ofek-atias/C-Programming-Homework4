68362867b85aa19553b4791cad0b2496d678771f
ofek-atias <ofeqi123@gmail.com>
2025-12-14 14:01:34 +0200

    Added part one of the project (15 points) Should be working, more tests are required

--- song.c
+++ song.c
@@ -5,29 +5,105 @@
 
 void set_song(Song *s, const char *title, const char *artist,
               int duration, Genre genre) {
+    
+    if (s == NULL) //Unsure
+    {
+        printf("ERROR: NULL Song pointer.\n");
+        exit(1);
+    }
+    if (title == NULL)
+    {
+        printf("ERROR: NULL title pointer.\n");
+        exit(1);
+    }
+    if (artist == NULL)
+    {    
+        printf("ERROR: NULL artist pointer.\n");
+        exit(1);
+    }
+    (s->title) = (char*) malloc(strlen(title)+1);
+    (s->artist) = (char*) malloc(strlen(artist)+1);
 
-    printf("ERROR: NULL Song pointer.\n");
+    if (s->title == NULL)
+    {
+        printf("ERROR: Failed to allocate memory for title.\n");
+        exit(1);
+    }
+    if (s-> artist == NULL)
+    {
+        printf("ERROR: Failed to allocate memory for artist.\n");
+        exit(1);
+    }
 
-    printf("ERROR: NULL title pointer.\n");
+    strcpy(s->title, title);
+    strcpy(s->artist, artist);
 
-    printf("ERROR: NULL artist pointer.\n");
-
-    printf("ERROR: Failed to allocate memory for title.\n");
-
-    printf("ERROR: Failed to allocate memory for artist.\n");
+    s-> duration_in_seconds = duration;
+    s-> genre = genre;
 }
 
 
-void print_song(const Song *s) {
-    printf("ERROR: NULL Song pointer.\n");
+void print_song(const Song *s) 
+{
+    char *genre_str;
 
-    printf("ERROR: Song has NULL fields.\n");
+    if (s == NULL)
+    {
+        printf("ERROR: NULL Song pointer.\n");
+        exit(1);
+    }
+    if ((s->title == NULL) || (s->artist == NULL))
+    {
+        printf("ERROR: Song has NULL fields.\n");
+        exit(1);
+    }
 
-    printf("Title: %s, Artist: %s, Duration: %ds, Genre: %s\n",
-           , , ,
-           );
+    switch(s->genre){
+        case POP:
+            genre_str = "POP";
+            break;
+        case ROCK:
+            genre_str = "ROCK";
+            break;
+        case JAZZ:
+            genre_str = "JAZZ";
+            break;
+        case CLASSICAL:
+            genre_str = "CLASSICAL";
+            break;
+        case HIPHOP:
+            genre_str = "HIPHOP";
+            break;
+    }
+    
+
+    printf("Title: %s, Artist: %s, Duration: %ds, Genre: %s\n", s->title, s->artist, s->duration_in_seconds, genre_str);
 }
 
-void free_song(Song *s) {
-    printf("ERROR: NULL Song pointer.\n");
+void free_song(Song *s) 
+{
+    if (s == NULL) //unsure
+    {
+        printf("ERROR: NULL Song pointer.\n");
+        exit(1);
+    }
+    if (s->title != NULL)
+        free(s->title);
+    if (s->artist != NULL)
+        free(s->artist);
+    free(s);
+}
+
+int main(){
+    
+    Song *song = (Song*)malloc(sizeof(Song));
+
+    const char *art = "Noam";
+    const char *name = "Luck";
+
+    set_song(song, art, name, 120, POP);
+    print_song(song);
+
+
+    return 0;
 }
